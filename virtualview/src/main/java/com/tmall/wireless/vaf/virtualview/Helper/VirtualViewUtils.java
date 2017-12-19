ae41e7b5ae3332b0e9b499ff0701f49ae6e40202
longerian <xlhongultimate@gmail.com>
2017-12-19 21:38:08 +0800

    support rp for lineWidth

--- virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/Helper/VirtualViewUtils.java
+++ virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/Helper/VirtualViewUtils.java
@@ -114,4 +114,58 @@ public class VirtualViewUtils {
         canvas.drawPath(sPath, backgroundPaint);
     }
 
+    public static void clipCanvas(Canvas canvas, int width, int height, int borderWidth,
+        int borderTopLeftRadius, int borderTopRightRadius, int borderBottomLeftRadius, int borderBottomRightRadius) {
+        if (canvas == null) {
+            return;
+        }
+        if (!isRounded(borderTopLeftRadius, borderTopRightRadius, borderBottomLeftRadius, borderBottomRightRadius)) {
+            return;
+        }
+
+        float halfBorderWidth = (borderWidth / 2.0f);
+        sPath.reset();
+        //start point
+        sPath.moveTo(borderWidth + (borderTopLeftRadius > 0 ? borderTopLeftRadius : 0), borderWidth);
+        //line top edge
+        sPath.lineTo(width - borderWidth - (borderTopRightRadius > 0 ? borderTopRightRadius : 0), borderWidth);
+        //arc to right edge
+        if (borderTopRightRadius > 0) {
+            oval.set(width - 2 * borderTopRightRadius, 0, width, 2 * borderTopRightRadius);
+            oval.offset(-halfBorderWidth, halfBorderWidth);
+            sPath.arcTo(oval, 270, 90);
+        }
+        //line right edge
+        sPath.lineTo(width - borderWidth, height - borderWidth - (borderBottomRightRadius > 0 ? borderBottomRightRadius : 0));
+        //arc to bottom edge
+        if (borderBottomRightRadius > 0) {
+            oval.set(width - 2 * borderBottomRightRadius, height - 2 * borderBottomRightRadius, width, height);
+            oval.offset(-halfBorderWidth, -halfBorderWidth);
+            sPath.arcTo(oval, 0, 90);
+        }
+        //line bottom edge
+        sPath.lineTo(borderWidth + (borderBottomLeftRadius > 0 ? borderBottomLeftRadius : 0), height - borderWidth);
+        //arc to left edge
+        if (borderBottomLeftRadius > 0) {
+            oval.set(0, height - 2 * borderBottomLeftRadius, 2 * borderBottomLeftRadius, height);
+            oval.offset(halfBorderWidth, -halfBorderWidth);
+            sPath.arcTo(oval, 90, 90);
+        }
+        //line left edge
+        sPath.lineTo(borderWidth, borderWidth + (borderTopLeftRadius > 0 ? borderTopLeftRadius : 0));
+        //arc to top edge
+        if (borderTopLeftRadius > 0) {
+            oval.set(0, 0, 2 * borderTopLeftRadius, 2 * borderTopLeftRadius);
+            oval.offset(halfBorderWidth, halfBorderWidth);
+            sPath.arcTo(oval, 180, 90);
+        }
+        canvas.clipPath(sPath);
+    }
+
+    private static boolean isRounded(int borderTopLeftRadius, int borderTopRightRadius, int borderBottomLeftRadius,
+        int borderBottomRightRadius) {
+        return borderTopLeftRadius > 0 || borderTopRightRadius > 0 || borderBottomLeftRadius > 0
+            || borderBottomRightRadius > 0;
+    }
+
 }
