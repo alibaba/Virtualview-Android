8513e1b194551e0b1af66b8fe92a87609985084d
longerian <xlhongultimate@gmail.com>
2018-03-11 22:23:01 +0800

    impl measure and layout, #13

--- virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/view/nlayout/NFrameLayoutImpl.java
+++ virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/view/nlayout/NFrameLayoutImpl.java
@@ -29,32 +29,33 @@ public class NFrameLayoutImpl extends NFrameLayoutView implements IView, IContai
 
     @Override
     public void measureComponent(int widthMeasureSpec, int heightMeasureSpec) {
+        this.measure(widthMeasureSpec, heightMeasureSpec);
 
     }
 
     @Override
     public void comLayout(int l, int t, int r, int b) {
-
+        this.layout(l, t, r, b);
     }
 
     @Override
     public void onComMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
+        this.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     public void onComLayout(boolean changed, int l, int t, int r, int b) {
-
+        this.onLayout(changed, l, t, r, b);
     }
 
     @Override
     public int getComMeasuredWidth() {
-        return 0;
+        return this.getMeasuredWidth();
     }
 
     @Override
     public int getComMeasuredHeight() {
-        return 0;
+        return this.getMeasuredHeight();
     }
 
     @Override
@@ -74,7 +75,7 @@ public class NFrameLayoutImpl extends NFrameLayoutView implements IView, IContai
 
     @Override
     public View getHolderView() {
-        return null;
+        return this;
     }
 
     @Override
@@ -84,6 +85,6 @@ public class NFrameLayoutImpl extends NFrameLayoutView implements IView, IContai
 
     @Override
     public int getType() {
-        return 0;
+        return -1;
     }
 }
--- virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/view/nlayout/NRatioLayoutImpl.java
+++ virtualview/src/main/java/com/tmall/wireless/vaf/virtualview/view/nlayout/NRatioLayoutImpl.java
@@ -29,32 +29,32 @@ public class NRatioLayoutImpl extends NRatioLayoutView implements IView, IContai
 
     @Override
     public void measureComponent(int widthMeasureSpec, int heightMeasureSpec) {
-
+        this.measure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     public void comLayout(int l, int t, int r, int b) {
-
+        this.layout(l, t, r, b);
     }
 
     @Override
     public void onComMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
+        this.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     public void onComLayout(boolean changed, int l, int t, int r, int b) {
-
+        this.onLayout(changed, l, t, r, b);
     }
 
     @Override
     public int getComMeasuredWidth() {
-        return 0;
+        return this.getMeasuredWidth();
     }
 
     @Override
     public int getComMeasuredHeight() {
-        return 0;
+        return this.getMeasuredHeight();
     }
 
     @Override
@@ -74,7 +74,7 @@ public class NRatioLayoutImpl extends NRatioLayoutView implements IView, IContai
 
     @Override
     public View getHolderView() {
-        return null;
+        return this;
     }
 
     @Override
@@ -84,6 +84,6 @@ public class NRatioLayoutImpl extends NRatioLayoutView implements IView, IContai
 
     @Override
     public int getType() {
-        return 0;
+        return -1;
     }
 }
